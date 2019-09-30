57d3e597415b2090fe060c8b2369764586bd4d4f
Prasetiyo Hadi Purwoko <prasetiyohadi92@gmail.com>
2019-09-30 21:01:36 +0700

    Multiple changes

--- aliyun/aliyun_cli.sh
+++ aliyun/aliyun_cli.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
 
 export ALIYUN_CLI_VERSION=3.0.26
--- minishift/minishift_cli.sh
+++ minishift/minishift_cli.sh
@@ -5,8 +5,7 @@ export MINISHIFT_VERSION=1.34.1
 
 # install minishift
 wget -O /tmp/minishift-${MINISHIFT_VERSION}-linux-amd64.tgz https://github.com/minishift/minishift/releases/download/v${MINISHIFT_VERSION}/minishift-${MINISHIFT_VERSION}-linux-amd64.tgz
-mkdir -p ~/bin
 tar -zxvf /tmp/minishift-${MINISHIFT_VERSION}-linux-amd64.tgz -C /tmp
-mv /tmp/minishift-${MINISHIFT_VERSION}-linux-amd64/minishift ~/bin
+sudo mv /tmp/minishift-${MINISHIFT_VERSION}-linux-amd64/minishift /usr/local/bin
 rm -frv /tmp/minishift-${MINISHIFT_VERSION}-linux-amd64
 rm -fv /tmp/minishift-${MINISHIFT_VERSION}-linux-amd64.tgz
