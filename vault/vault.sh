49d219e35f477881f98865c0082316e6a4ff101c
Prasetiyo Hadi Purwoko <prasetiyohadi92@gmail.com>
2019-10-23 16:24:54 +0700

    Update vault version

--- vault/vault.sh
+++ vault/vault.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-export VAULT_VERSION=1.2.2
+export VAULT_VERSION=1.2.3
 
 # install vault
 wget -O /tmp/vault_${VAULT_VERSION}_linux_amd64.zip https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
