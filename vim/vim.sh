409e12e9d3044dfe78e6245fffb191627dbfde1d
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-01-18 14:10:22 +0700

    Update vim setup script

--- vim/vim.sh
+++ vim/vim.sh
@@ -2,16 +2,24 @@
 set -ex
 
 # install .vimrc
-CWD=$(dirname $(realpath $0))
+CWD=$(dirname $0)
 cp $CWD/vimrc ~/.vimrc
 
-# install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# install vim packages
+mkdir -p ~/.vim/pack/plugins/start
+git clone https://github.com/scrooloose/nerdtree.git \
+  ~/.vim/pack/plugins/start/nerdtree
+git clone https://github.com/vim-airline/vim-airline \
+  ~/.vim/pack/plugins/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes \
+  ~/.vim/pack/plugins/start/vim-airline-themes
+git clone https://github.com/tpope/vim-fugitive.git \
+  ~/.vim/pack/plugins/start/vim-fugitive
+git clone https://github.com/airblade/vim-gitgutter.git \
+  ~/.vim/pack/plugins/start/vim-gitgutter
+git clone https://github.com/tpope/vim-sensible.git \
+  ~/.vim/pack/plugins/start/vim-sensible
+git clone https://github.com/jmcantrell/vim-virtualenv.git \
+  ~/.vim/pack/plugins/start/vim-virtualenv
 
-# clone vim plugins
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
-git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
-git clone https://github.com/plytophogy/vim-virtualenv.git ~/.vim/bundle/vim-virtualenv
+# do not forget to run :helptags ~/.vim/pack/plugins/start/vim-airline/doc
