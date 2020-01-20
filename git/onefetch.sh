ac63b2b273fa5338c7042a63c442d17d9f01d38b
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-01-20 10:47:44 +0700

    Update onefetch.sh

--- git/onefetch.sh
+++ git/onefetch.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -ex
 
-export ONEFETCH_VERSION=v1.6.5
+export OS=${OSTYPE:-'linux'}
+export OS_TYPE=linux
+export ONEFETCH_VERSION=v2.2.0
+export ONEFETCH_URL=https://github.com/o2sh/onefetch/releases/download/${ONEFETCH_VERSION}/onefetch_${OS_TYPE}_x86-64.zip
 
 # install onefetch
-wget -O /tmp/onefetch_linux_x86-64.zip https://github.com/o2sh/onefetch/releases/download/${ONEFETCH_VERSION}/onefetch_linux_x86-64.zip
+wget -O /tmp/onefetch_${OS_TYPE}_x86-64.zip https://github.com/o2sh/onefetch/releases/download/${ONEFETCH_VERSION}/onefetch_${OS_TYPE}_x86-64.zip
 mkdir -p ~/bin
-unzip /tmp/onefetch_linux_x86-64.zip -d ~/bin
-rm -f /tmp/onefetch_linux_x86-64.zip
+unzip /tmp/onefetch_${OS_TYPE}_x86-64.zip -d ~/bin
+rm -f /tmp/onefetch_${OS_TYPE}_x86-64.zip
