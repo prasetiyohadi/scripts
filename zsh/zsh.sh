d81cf96fca669e58f23a9537568ee4ea2792dc05
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-01-20 10:46:55 +0700

    Update zsh script and theme

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -2,15 +2,17 @@
 set -ex
 
 # install .zshrc
-CWD=$(dirname $(realpath $0))
+CWD=$(dirname $0)
 cp $CWD/zshrc ~/.zshrc
 
 # install oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # install oh-my-zsh custom plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # install oh-my-zsh custom themes
 cp $CWD/catalyst.zsh-theme ~/.oh-my-zsh/custom/themes/catalyst.zsh-theme
