1983d93f7397b93d8fc3a0c7844e318ba2a433aa
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-02-08 16:18:28 +0700

    Update gcloud and zsh scripts

--- gcloud/gcloud.sh
+++ gcloud/gcloud.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 set -ex
 
-export GCLOUD_SDK_ROOT=~/.local/google-cloud-sdk
+export GCLOUD_SDK_ROOT=$HOME/.local/google-cloud-sdk
 export GCLOUD_SDK_URL=https://dl.google.com/dl/cloudsdk/channels/rapid/downloads
-export GCLOUD_SDK_VERSION=276.0.0
+export GCLOUD_SDK_VERSION=279.0.0
 
 if [ ! -f /tmp/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz ];
 then
   wget -O /tmp/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz \
     ${GCLOUD_SDK_URL}/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz
 fi
-mkdir -p ~/.local
+mkdir -p $HOME/.local
 tar -xf /tmp/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz \
-  -C ~/.local
+  -C $HOME/.local
 sh ${GCLOUD_SDK_ROOT}/install.sh --quiet
-rm -f /tmp/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz
+sh ${GCLOUD_SDK_ROOT}/bin/gcloud -q components update
+sh ${GCLOUD_SDK_ROOT}/bin/gcloud -q components install kubectl
+
+#rm -f /tmp/google-cloud-sdk-${GCLOUD_SDK_VERSION}-linux-x86_64.tar.gz
