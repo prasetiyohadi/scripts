2257e936931653d388fbc5089e6c12ea49428dd0
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-04-09 11:37:52 +0700

    Update go version

--- go/go.sh
+++ go/go.sh
@@ -6,7 +6,7 @@ export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
 [[ "$OS_TYPE" == "darwin" ]] && export OS_TYPE=darwin-amd64
 [[ "$OS_TYPE" == "linux-gnu" ]] && export OS_TYPE=linux-amd64
 [[ "$OS_TYPE" == "linux-gnueabihf" ]] && export OS_TYPE=linux-armv6l
-export GO_VERSION=1.13.7
+export GO_VERSION=1.14.2
 export GO_URL=https://dl.google.com/go/go${GO_VERSION}.${OS_TYPE}.tar.gz
 export GO_INSTALL_PATH=~/.local
 export GOROOT=~/.local/go
