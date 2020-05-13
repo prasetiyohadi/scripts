00dcb1ab8cc1de20cdd1dcb08341a8cc37445d21
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-05-13 23:00:24 +0700

    Update openvpn script

--- openvpn/openvpn.sh
+++ openvpn/openvpn.sh
@@ -3,5 +3,9 @@ set -ex
 
 sudo apt update
 sudo apt install openvpn
-sudo mv -f /etc/openvpn{,.orig}
-sudo ln -s ~/openvpn /etc/openvpn
+if [ -d ~/openvpn ]; then
+  sudo mv -f /etc/openvpn{,.orig}
+  sudo ln -s ~/openvpn /etc/openvpn
+  sudo systemctl disable systemd-ask-password-wall.path systemd-ask-password-wall.service
+  sudo systemctl stop systemd-ask-password-wall.path systemd-ask-password-wall.service
+fi
