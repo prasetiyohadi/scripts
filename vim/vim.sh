22a19aa077265090b41e2ea944a91ee7d8553332
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-05-13 23:11:01 +0700

    Update vim scripts

--- vim/vim.sh
+++ vim/vim.sh
@@ -7,8 +7,12 @@ cp $CWD/vimrc ~/.vimrc
 
 # install vim packages
 mkdir -p ~/.vim/pack/plugins/start
+git clone https://github.com/Shougo/deoplete.nvim \
+  ~/.vim/pack/plugins/start/deoplete
 git clone https://github.com/scrooloose/nerdtree.git \
   ~/.vim/pack/plugins/start/nerdtree
+git clone https://github.com/roxma/nvim-yarp \
+  ~/.vim/pack/plugins/start/nvim-yarp
 git clone https://github.com/vim-airline/vim-airline \
   ~/.vim/pack/plugins/start/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes \
@@ -17,6 +21,10 @@ git clone https://github.com/tpope/vim-fugitive.git \
   ~/.vim/pack/plugins/start/vim-fugitive
 git clone https://github.com/airblade/vim-gitgutter.git \
   ~/.vim/pack/plugins/start/vim-gitgutter
+git clone https://github.com/fatih/vim-go.git \
+  ~/.vim/pack/plugins/start/vim-go
+git clone https://github.com/roxma/vim-hug-neovim-rpc \
+  ~/.vim/pack/plugins/start/vim-hug-neovim-rpc
 git clone https://github.com/tpope/vim-sensible.git \
   ~/.vim/pack/plugins/start/vim-sensible
 git clone https://github.com/jmcantrell/vim-virtualenv.git \
