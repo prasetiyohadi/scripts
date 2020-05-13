bb421794db26cb118a297cf59c3fb063966d670c
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-05-13 22:45:06 +0700

    Update consul version

--- consul/consul.sh
+++ consul/consul.sh
@@ -6,7 +6,7 @@ export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
 [[ "$OS_TYPE" == "darwin" ]] && export OS_TYPE=darwin_amd64
 [[ "$OS_TYPE" == "linux-gnu" ]] && export OS_TYPE=linux_amd64
 [[ "$OS_TYPE" == "linux-gnueabihf" ]] && export OS_TYPE=linux_armhfv6
-export CONSUL_VERSION=1.6.2
+export CONSUL_VERSION=1.7.2
 export CONSUL_PKG=consul_${CONSUL_VERSION}_${OS_TYPE}.zip
 export CONSUL_URL=https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_${OS_TYPE}.zip
 
