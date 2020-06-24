51c725b22fc10d50a8ebe678f9bbb34b74032ec4
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-06-24 22:43:47 +0700

    Add script to install protobuf compiler

---
+++ protobuf/protoc3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+export OS=${OSTYPE:-'linux-gnu'}
+export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
+[[ "$OS_TYPE" == "darwin" ]] && export OS_TYPE=osx-x86_64
+[[ "$OS_TYPE" == "linux-gnu" ]] && export OS_TYPE=linux-x86_64
+export PROTOC3_VERSION=3.12.3
+export PROTOC3_PKG=protoc-${PROTOC3_VERSION}-${OS_TYPE}.zip
+export PROTOC3_URL=https://github.com/protocolbuffers/protobuf
+export PROTOC3_URL=${PROTOC3_URL}/releases/download
+export PROTOC3_URL=${PROTOC3_URL}/v${PROTOC3_VERSION}/${PROTOC3_PKG}
+
+# install protobuf compiler
+cd "$(mktemp -d)"
+curl -fsSLO "${PROTOC3_URL}"
+unzip ${PROTOC3_PKG} -d protoc3
+sudo mv protoc3/bin/* /usr/local/bin/
+sudo mv protoc3/include/* /usr/local/include/
+sudo chown $USER /usr/local/bin/protoc
+sudo chown -R $USER /usr/local/include/google
+
+# test
+protoc --version
