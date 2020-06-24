2a1d66899c6baeccc6b3f26b1a1048efddd556e5
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-06-24 11:50:00 +0700

    Add script to install krew

---
+++ kubernetes/krew.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ex
+
+KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64"
+KREW_ROOT=$HOME
+
+cd "$(mktemp -d)"
+curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}"
+tar zxvf krew.tar.gz
+"$KREW" install --manifest=krew.yaml --archive=krew.tar.gz
+"$KREW" update
+
+# Add $HOME/.krew/bin directory to your PATH environment variable
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
