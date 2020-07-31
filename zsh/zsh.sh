eacc385fb326bf13b5e78443ace6043f243bb0c8
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-07-31 15:37:03 +0700

    Update zsh scripts

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-set -ex
+#!/bin/bash
+set -euxo pipefail
 
 # install .zshrc
 CWD=$(dirname $0)
@@ -8,13 +8,25 @@ if [ -f /etc/debian_version ]; then
 fi
 
 # install oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+if [ -d ~/.oh-my-zsh ]; then
+  (cd ~/.oh-my-zsh && git pull)
+else
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
 
 # install oh-my-zsh custom plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions \
-  ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-  ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+  (cd ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && git pull)
+else
+  git clone https://github.com/zsh-users/zsh-autosuggestions \
+    ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+  (cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull)
+else
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
 
 # install oh-my-zsh custom themes
 cp $CWD/catalyst.zsh-theme ~/.oh-my-zsh/custom/themes/catalyst.zsh-theme
