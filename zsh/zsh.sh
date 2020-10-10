221917eda8ec844cc56c377e702e8da2c963476d
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-10-10 13:51:16 +0700

    Update zsh scripts

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -8,31 +8,33 @@ export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
 CWD=$(dirname $0)
 if [ "${OS_TYPE}" == "linux-gnu" ]; then
     if [ -f /etc/debian_version ]; then
-      cp $CWD/zshrc.debian ~/.zshrc
+        cp $CWD/zshrc.debian ~/.zshrc
+    elif [ -f /etc/redhat-release ]; then
+        cp $CWD/zshrc.redhat ~/.zshrc
     fi
 elif [ "${OS_TYPE}" == "darwin" ]; then
-  cp $CWD/zshrc.macos ~/.zshrc
+    cp $CWD/zshrc.macos ~/.zshrc
 fi
 
 # install oh-my-zsh
 if [ -d ~/.oh-my-zsh ]; then
-  (cd ~/.oh-my-zsh && git pull)
+    (cd ~/.oh-my-zsh && git pull)
 else
-  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 
 # install oh-my-zsh custom plugins
 if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
-  (cd ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && git pull)
+    (cd ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions && git pull)
 else
-  git clone https://github.com/zsh-users/zsh-autosuggestions \
-    ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions \
+        ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-  (cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull)
+    (cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull)
 else
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
 # install oh-my-zsh custom themes
