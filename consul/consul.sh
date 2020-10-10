b9f1796ece6fccd5e1843b7ba23202dbd80a0e53
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-10-10 13:02:09 +0700

    Bulk update from vector

--- consul/consul.sh
+++ consul/consul.sh
@@ -6,9 +6,10 @@ export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
 [[ "$OS_TYPE" == "darwin" ]] && export OS_TYPE=darwin_amd64
 [[ "$OS_TYPE" == "linux-gnu" ]] && export OS_TYPE=linux_amd64
 [[ "$OS_TYPE" == "linux-gnueabihf" ]] && export OS_TYPE=linux_armhfv6
-export CONSUL_VERSION=1.8.3
+export CONSUL_VERSION=1.8.4
 export CONSUL_PKG=consul_${CONSUL_VERSION}_${OS_TYPE}.zip
-export CONSUL_URL=https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_${OS_TYPE}.zip
+export CONSUL_URL=https://releases.hashicorp.com/consul/${CONSUL_VERSION}
+export CONSUL_URL=${CONSUL_URL}/${CONSUL_PKG}
 
 # install consul
 wget -O /tmp/${CONSUL_PKG} ${CONSUL_URL}
---
+++ postgresql/postgresql.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export OS=${OSTYPE:-'linux-gnu'}
+export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
+
+if [ "${OS_TYPE}" == "linux-gnu" ]; then
+    if [ -f /etc/debian_version ]; then
+        sudo apt-get update
+        sudo apt-get install -y postgresql postgresql-client
+    fi
+fi
