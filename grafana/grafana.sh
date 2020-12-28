0e18f204aea53bd6c1f2ce5d9dbc2dcd9ad0471c
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2020-12-28 12:43:41 +0700

    Update grafana.sh script

--- grafana/grafana.sh
+++ grafana/grafana.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -euo pipefail
 
 export OS=${OSTYPE:-'linux-gnu'}
 export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
@@ -8,10 +8,11 @@ if [ "${OS_TYPE}" == "linux-gnu" ]; then
     if [ -f /etc/debian_version ]; then
         # install prerequisites
         sudo apt-get update
-        sudo apt-get install -y apt-transport-https software-properties-common wget
-        wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-        echo "deb https://packages.grafana.com/oss/deb stable main" | \
-            sudo tee -a /etc/apt/sources.list.d/grafana.list 
+        sudo apt-get install --assume-yes apt-transport-https software-properties-common wget
+        wget --quiet --output-document=- https://packages.grafana.com/gpg.key \
+            | sudo apt-key add -
+        echo "deb https://packages.grafana.com/oss/deb stable main" \
+            | sudo tee -a /etc/apt/sources.list.d/grafana.list 
         sudo apt-get update
         sudo apt-get install -y grafana
     fi
