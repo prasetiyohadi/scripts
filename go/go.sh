49bba5f7f125fef0f24a93be42515e12311b9ddf
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-04-04 10:33:54 +0700

    Refactor go installation script

--- go/go.sh
+++ go/go.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export OS=${OSTYPE:-'linux-gnu'}
-export OS_TYPE=`echo ${OS} | tr -d "[:digit:]"`
+export OS="${OSTYPE:-'linux-gnu'}"
+export OS_TYPE="$(echo "$OS" | tr -d "[:digit:]")"
 [[ "$OS_TYPE" == "darwin" ]] && export OS_TYPE=darwin-amd64
 [[ "$OS_TYPE" == "linux-gnu" ]] && export OS_TYPE=linux-amd64
 [[ "$OS_TYPE" == "linux-gnueabihf" ]] && export OS_TYPE=linux-armv6l
-export GO_VERSION=${GO_VERSION:-$(curl -s https://golang.org/VERSION?m=text)}
-export GO_URL=https://dl.google.com/go/${GO_VERSION}.${OS_TYPE}.tar.gz
+export GO_VERSION="${GO_VERSION:-$(curl -s https://golang.org/VERSION?m=text)}"
 export GO_INSTALL_PATH=~/.local
-export GOROOT=~/.local/go
+export GO_URL="https://dl.google.com/go/$GO_VERSION.$OS_TYPE.tar.gz"
 export GOPATH=~/go
+export GOROOT=~/.local/go
 
-echo "This script will install Go language version ${GO_VERSION}."
+echo "This script will install Go language version $GO_VERSION to $GOROOT."
 
-if [ "${OS_TYPE}" == "linux-amd64" ]; then
-    # install go
-    if [ -d ${GOROOT} ]; then
-        read -p "${GOROOT} already exists. Replace[yn]? " -n 1 -r
+# install go
+# https://golang.org/dl/
+
+install() {
+    mkdir -p "$GO_INSTALL_PATH" "$GOPATH"
+    wget -O - "$GO_URL" | tar -C "$GO_INSTALL_PATH" -zxf -
+}
+
+if [[ "$OS_TYPE" == "linux-amd64" ]]; then
+    if [[ -d "$GOROOT" ]]; then
+        read -p "$GOROOT already exists. Replace[yn]? " -n 1 -r
         echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            rm -r ${GOROOT}
-            mkdir -p ${GO_INSTALL_PATH} ${GOPATH}
-            wget -O - ${GO_URL} | tar -C ${GO_INSTALL_PATH} -zxf -
+        if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+            rm -r "$GOROOT"
+            install
         fi
     else
-        mkdir -p ${GO_INSTALL_PATH} ${GOPATH}
-        wget -O - ${GO_URL} | tar -C ${GO_INSTALL_PATH} -zxf -
+        install
     fi
 fi
