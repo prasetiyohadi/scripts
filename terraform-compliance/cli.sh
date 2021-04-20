a8859fc1a5e9ef681b3723a2c7a23940a5753f07
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-04-20 11:41:23 +0700

    Add terraform-compliance

---
+++ terraform-compliance/cli.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# install terraform-compliance
+# https://github.com/terraform-compliance/cli
+echo "This script will install terraform-compliance."
+pip3 install terraform-compliance
