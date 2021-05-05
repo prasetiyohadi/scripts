d263efdf95e62f115b2e10023c767161c14bf936
Prasetiyo Hadi Purwoko <pras@deuterion.net>
2021-05-05 10:34:49 +0700

    Update julia/julia.sh

--- julia/julia.sh
+++ julia/julia.sh
@@ -1,62 +1,64 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export OS="${OSTYPE:-'linux-gnu'}"
+OS="${OSTYPE:-'linux-gnu'}"
 OS_TYPE=$(echo "$OS" | tr -d ".[:digit:]")
-export OS_TYPE
-export JULIA_VERSION=1.6.0
-export JULIA_MAJOR_VERSION=1.6
-export JULIA_INSTALL_PATH=~/.local
-export JULIAROOT=~/.local/julia
+OS_TYPE_DARWIN=mac64
+OS_TYPE_LINUX_AMD64=linux-x86_64
+OS_TYPE_LINUX_ARM=linux-armv7l
+[ "$OS_TYPE" == "darwin" ] && export OS_TYPE="$OS_TYPE_DARWIN"
+[ "$OS_TYPE" == "linux-gnu" ] && export OS_TYPE="$OS_TYPE_LINUX_AMD64"
+[ "$OS_TYPE" == "linux-gnueabihf" ] && export OS_TYPE="$OS_TYPE_LINUX_ARM"
+APP_BIN=julia
+APP_VERSION=1.6.1
+APP_MAJOR_VERSION=1.6
+APP_SRC="$APP_BIN-$APP_VERSION-$OS_TYPE"
+APP_URL=https://julialang-s3.julialang.org/bin
+[ "$OS_TYPE" == "$OS_TYPE_DARWIN" ] \
+    && export APP_PKG="$APP_SRC.dmg" APP_URL="$APP_URL/mac/x64"
+[ "$OS_TYPE" == "$OS_TYPE_LINUX_AMD64" ] \
+    && export APP_PKG="$APP_SRC.tar.gz" APP_URL="$APP_URL/linux/x64"
+[ "$OS_TYPE" == "$OS_TYPE_LINUX_ARM" ] \
+    && export APP_PKG="$APP_SRC.tar.gz" APP_URL="$APP_URL/linux/armv7l"
+APP_URL="$APP_URL/$APP_MAJOR_VERSION/$APP_PKG"
+APP_INSTALL_PATH=~/.local
+APP_ROOT_PATH=~/.local/julia
 
-echo "This script will install Julia language version $JULIA_VERSION."
+setup() {
+    mkdir -p "$APP_INSTALL_PATH"
+    wget -O - "$APP_URL" | tar -C "$APP_INSTALL_PATH" -zxf -
+    mv "$APP_INSTALL_PATH/$APP_BIN-$APP_VERSION" \
+        "$APP_INSTALL_PATH/$APP_BIN"
+}
 
-if [ "$OS_TYPE" == "linux-gnu" ]; then
-    export JULIA_URL=https://julialang-s3.julialang.org/bin/linux/x64
-    export JULIA_URL="$JULIA_URL/$JULIA_MAJOR_VERSION"
-    export JULIA_URL="$JULIA_URL/julia-$JULIA_VERSION-linux-x86_64.tar.gz"
-    # install julia
-    if [ -d "$JULIAROOT" ]; then
-        read -p "$JULIAROOT already exists. Replace[yn]? " -n 1 -r
+main_linux() {
+    echo "This script will install $APP_BIN version $APP_VERSION."
+    if [ -d "$APP_ROOT_PATH" ]; then
+        read -p "$APP_ROOT_PATH already exists. Replace[yn]? " -n 1 -r
         echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            rm -rf "$JULIAROOT"
-            mkdir -p "$JULIA_INSTALL_PATH"
-            wget -O - "$JULIA_URL" | tar -C "$JULIA_INSTALL_PATH" -zxf -
-            mv "$JULIA_INSTALL_PATH/julia-$JULIA_VERSION" \
-                "$JULIA_INSTALL_PATH/julia"
+        if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+            rm -rf "$APP_ROOT_PATH"
+            setup
+        else
+            echo "Installation cancelled."
         fi
     else
-        mkdir -p "$JULIA_INSTALL_PATH"
-        wget -O - "$JULIA_URL" | tar -C "$JULIA_INSTALL_PATH" -zxf -
-        mv "$JULIA_INSTALL_PATH/julia-$JULIA_VERSION" \
-            "$JULIA_INSTALL_PATH/julia"
+        setup
     fi
-elif [ "$OS_TYPE" == "linux-gnueabihf" ]; then
-    export JULIA_URL=https://julialang-s3.julialang.org/bin/linux/aarch64
-    export JULIA_URL="$JULIA_URL/$JULIA_MAJOR_VERSION"
-    export JULIA_URL="$JULIA_URL/julia-$JULIA_VERSION-linux-aarch64.tar.gz"
-    # install julia
-    if [ -d "$JULIAROOT" ]; then
-        read -p "$JULIAROOT already exists. Replace[yn]? " -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            rm -rf "$JULIAROOT"
-            mkdir -p "$JULIA_INSTALL_PATH"
-            wget -O - "$JULIA_URL" | tar -C "$JULIA_INSTALL_PATH" -zxf -
-            mv "$JULIA_INSTALL_PATH/julia-$JULIA_VERSION" \
-                "$JULIA_INSTALL_PATH/julia"
-        fi
-    else
-        mkdir -p "$JULIA_INSTALL_PATH"
-        wget -O - "$JULIA_URL" | tar -C "$JULIA_INSTALL_PATH" -zxf -
-        mv "$JULIA_INSTALL_PATH/julia-$JULIA_VERSION" \
-            "$JULIA_INSTALL_PATH/julia"
+}
+
+main_darwin() {
+    echo "This script will download $APP_BIN version $APP_VERSION."
+    wget -O "$HOME/Downloads/$APP_PKG" "$APP_URL"
+}
+
+main() {
+    if [ "$OS_TYPE" == "$OS_TYPE_LINUX_AMD64" ] \
+        || [ "$OS_TYPE" == "$OS_TYPE_LINUX_ARM" ]; then
+        main_linux
+    elif [ "$OS_TYPE" == "$OS_TYPE_DARWIN" ]; then
+        main_darwin
     fi
-elif [ "$OS_TYPE" == "darwin" ]; then
-    export JULIA_URL=https://julialang-s3.julialang.org/bin/mac/x64
-    export JULIA_URL="$JULIA_URL/$JULIA_MAJOR_VERSION"
-    export JULIA_URL="$JULIA_URL/julia-$JULIA_VERSION-mac64.dmg"
-    export JULIA_PKG="julia-$JULIA_VERSION-mac64.dmg"
-    wget -O ~/"$JULIA_PKG" "$JULIA_URL"
-fi
+}
+
+main
