3af81038a4972e491ce6cfd2efd4f3a738b18de5
Prasetiyo Hadi Purwoko <prasetiyohadi92@gmail.com>
2025-02-07 09:35:49 +0700

    update(yq): bump version

--- yq/yq.sh
+++ yq/yq.sh
@@ -8,48 +8,48 @@ OS_TYPE_LINUX_AMD64=linux_amd64
 [ "$OS_TYPE" == "linux-gnu" ] && export OS_TYPE=$OS_TYPE_LINUX_AMD64
 APP_BIN=yq
 APP_URL=https://github.com/mikefarah/yq/releases/download
-APP_VERSION=4.9.6
-APP_PATH=~/bin/$APP_BIN
+APP_VERSION=4.44.2
+APP_PATH=~/.local/bin/$APP_BIN
 APP_SRC=${APP_BIN}_${OS_TYPE}
 APP_URL=$APP_URL/v$APP_VERSION/$APP_SRC
 
 check_version() {
-    $APP_BIN --version
+  $APP_BIN --version
 }
 
 install_linux() {
-    mkdir -p ~/bin
-    wget -O ~/bin/$APP_BIN $APP_URL
-    chmod +x ~/bin/$APP_BIN
+  mkdir -p ~/.local/bin
+  wget -O ~/.local/bin/$APP_BIN $APP_URL
+  chmod +x ~/.local/bin/$APP_BIN
 }
 
 setup_darwin() {
-    echo "This script will install $APP_BIN using brew."
-    command -v brew > /dev/null && brew install $APP_BIN
+  echo "This script will install $APP_BIN using brew."
+  command -v brew >/dev/null && brew install $APP_BIN
 }
 
 setup_linux() {
-    echo "This script will install $APP_BIN version $APP_VERSION."
-    if [ -s "$APP_PATH" ]; then
-        check_version
-        read -p "$APP_PATH already exists. Replace[yn]? " -n 1 -r
-        echo
-        if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-            install_linux
-        else
-            echo "Installation cancelled."
-        fi
+  echo "This script will install $APP_BIN version $APP_VERSION."
+  if [ -s "$APP_PATH" ]; then
+    check_version
+    read -p "$APP_PATH already exists. Replace[yn]? " -n 1 -r
+    echo
+    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+      install_linux
     else
-        install_linux
+      echo "Installation cancelled."
     fi
+  else
+    install_linux
+  fi
 }
 
 main() {
-    if [ "$OS_TYPE" == "$OS_TYPE_DARWIN" ]; then
-        setup_darwin
-    elif [ "$OS_TYPE" == "$OS_TYPE_LINUX_AMD64" ]; then
-        setup_linux
-    fi
+  if [ "$OS_TYPE" == "$OS_TYPE_DARWIN" ]; then
+    setup_darwin
+  elif [ "$OS_TYPE" == "$OS_TYPE_LINUX_AMD64" ]; then
+    setup_linux
+  fi
 }
 
 main
